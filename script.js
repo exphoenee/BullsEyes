6108ca40ed46dcdbd02860d6d1951797840f2960
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-02-16 14:02:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,13 @@
-window.addEventListener('load', function() {
-  const canvas = document.getElementById('canvas1');
-  const ctx = canvas.getContext('2d');
-  canvas.width = 1280;
-  canvas.height = 720;
+import Game from "./src/Game.js";
 
-  class Player {
+window.addEventListener("load", function () {
+  const game = new Game();
+  game.init();
+  console.log(game);
+  game.render();
 
-    constructor(game) {
-      if (typeof Player.instance === "object") {
-        return Player.instance;
-      }
-      Player.instance = this;
-      this.game = game;
-    }
-  }
-
-  class Game {
-
-    constructor(canvas) {
-      if (typeof Game.instance === "object") {
-        return Game.instance;
-      }
-      Game.instance = this;
-      this.canvas = canvas;
-      this.width = this.canvas.width;
-      this.height = this.canvas.height;
-      this.player = new Player(this);
-    }
-  }
-
-  const game = new Game(canvas);
-
-  function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    requestAnimationFrame(animate);
-  }
+  (function animate() {
+    game.render();
+    window.requestAnimationFrame(animate);
+  })();
 });
