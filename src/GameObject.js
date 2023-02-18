5302c472e7f5ea9da2d8bda5da30b06cc374675c
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-02-18 17:26:50 +0100

    Update GameObject.js

--- src/GameObject.js
+++ src/GameObject.js
@@ -78,4 +78,90 @@ class GameObject {
     this.spriteX = this.collisionX - this.width * this.spriteOffsetX;
     this.spriteY = this.collisionY - this.height * this.spriteOffsetY;
   }
+
+  initPosition() {
+    this.collisionX = this.game.width * 0.5;
+    this.collisionY = this.game.height * 0.5;
+  }
+
+  areYou(name) {
+    return this.name === name;
+  }
+
+  draw() {
+    this.game.context.drawImage(
+      this.image,
+      this.animationFrame * this.spriteWidth,
+      this.spriteDirection * this.spriteHeight,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.spriteX,
+      this.spriteY,
+      this.width,
+      this.height
+    );
+    this.drwaHitbox();
+  }
+
+  drawHitbox() {
+    if (this.game.debug) {
+      this.game.context.beginPath();
+      this.game.context.arc(
+        this.collisionX,
+        this.collisionY,
+        this.collisionRadius,
+        0,
+        Math.PI * 2,
+        false
+      );
+      this.game.context.save();
+      this.game.context.globalAlpha = this.collisionOpacity;
+      this.game.context.fill();
+      this.game.context.restore();
+      this.game.context.stroke();
+      this.game.context.beginPath();
+      this.game.context.moveTo(this.collisionX, this.collisionY);
+      this.game.context.lineTo(this.game.mouse.x, this.game.mouse.y);
+      this.game.context.stroke();
+    }
+  }
+  collision() {
+    console.warn("collision() not implemented");
+  }
+
+  pushObject({ distance, sumOfRadii, dx, dy }) {
+    this.collisionX -= (dx / distance) * (sumOfRadii - distance);
+    this.collisionY -= (dy / distance) * (sumOfRadii - distance);
+  }
+
+  objectDirection() {
+    const angleStep = 360 / this.ainmationDirection;
+
+    const angle = Math.floor(
+      ((Math.atan2(
+        this.game.mouse.y - this.collisionY,
+        this.game.mouse.x - this.collisionX
+      ) *
+        360) /
+        (2 * Math.PI) +
+        90 +
+        360) %
+        360
+    );
+
+    this.spriteDirection = Math.floor(angle / angleStep);
+  }
+
+  objectMove() {
+    console.warn("objectMove() not implemented");
+  }
+
+  update() {
+    this.draw();
+    this.objectMove();
+    this.collision();
+    this.objectDirection();
+  }
 }
+
+export default GameObject;
