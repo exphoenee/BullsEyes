89cdf9303a6215c2296fee6c124aae627a52107e
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-02-18 20:27:04 +0100

    Update Enemy.js

--- src/Enemy.js
+++ src/Enemy.js
@@ -3,7 +3,7 @@ import { v4 as uuid } from "uuid";
 import GameObject from "./GameObject";
 import Sparks from "./Sparks";
 
-import { enemy } from "./constants/names";
+import { enemy, player, obstacle, egg } from "./constants/names";
 
 class Enemy extends GameObject {
   constructor(game) {
@@ -18,6 +18,7 @@ class Enemy extends GameObject {
         variant: Math.floor(Math.random() * 4),
       },
       collisionProperties: {
+        gameObjectNames: [obstacle, player],
         collisionRadius: 50,
       },
       motionSettings: {
@@ -36,106 +37,10 @@ class Enemy extends GameObject {
     this.opacityModifier = 0.1;
   }
 
-  areYou(name) {
-    return this.name === name;
-  }
-
   initPosition() {
     this.collisionX = this.game.width + this.width;
     this.collisionY =
       this.game.topMargin + Math.random() * (this.game.height - this.height);
-  }
-
-  draw() {
-    this.game.context.save();
-    this.game.context.globalAlpha = this.opacity;
-    this.game.context.drawImage(
-      this.image,
-      this.animationFrame * this.spriteWidth,
-      this.spriteDirection * this.spriteHeight,
-      this.spriteWidth,
-      this.spriteHeight,
-      this.spriteX,
-      this.spriteY,
-      this.width,
-      this.height
-    );
-    this.game.context.restore();
-    this.drwaHitbox();
-  }
-
-  drwaHitbox() {
-    if (this.game.debug) {
-      this.game.context.beginPath();
-      this.game.context.arc(
-        this.collisionX,
-        this.collisionY,
-        this.collisionRadius,
-        0,
-        Math.PI * 2,
-        false
-      );
-      this.game.context.save();
-      this.game.context.globalAlpha = this.collisionOpacity;
-      this.game.context.fill();
-      this.game.context.restore();
-      this.game.context.stroke();
-    }
-  }
-
-  collision() {
-    [...this.game.obstacles, this.game.player].forEach((object) => {
-      const collisionInfo = this.game.checkCollision(this, object) || {};
-      const { collision } = collisionInfo;
-
-      if (collision) {
-        this.pushObject(collisionInfo);
-      }
-    });
-  }
-
-  pushObject({ dx, dy, distance, sumOfRadii }) {
-    this.collisionX -= (dx / distance) * (sumOfRadii - distance);
-    this.collisionY -= (dy / distance) * (sumOfRadii - distance);
-  }
-
-  objectDirection() {
-    const angleStep = 360 / 8;
-
-    const angle = Math.floor(
-      ((Math.atan2(
-        this.game.mouse.y - this.collisionY,
-        this.game.mouse.x - this.collisionX
-      ) *
-        360) /
-        (2 * Math.PI) +
-        90 +
-        360) %
-        360
-    );
-
-    this.spriteDirection = Math.floor(angle / angleStep);
-  }
-
-  addSpark() {
-    const position = {
-      x: this.collisionX,
-      y: this.collisionY,
-    };
-    for (let i = 0; i < this.numberOfSparks; i++) {
-      this.game.particles.push(
-        new Sparks(this.game, position, this.colorOfSparks)
-      );
-    }
-  }
-
-  removeObject() {}
-
-  reduceOpacity() {
-    this.opacity =
-      this.opacity - this.opacityModifier < 0.1
-        ? 0
-        : this.opacity - this.opacityModifier;
   }
 
   init() {
@@ -144,18 +49,9 @@ class Enemy extends GameObject {
     this.lifeTime = 0;
   }
 
-  kill() {
-    this.reduceOpacity();
-    if (this.opacity <= 0) {
-      this.addSpark();
-      this.init();
-    }
-  }
-
   objectMove() {
     this.collisionX -= this.speedX * this.speedModifier;
     this.collisionY -= this.speedY * this.speedModifier;
-
     if (this.spriteX + this.width < 0) this.init();
 
     if (this.collisionX > this.game.width - this.collisionRadius)
